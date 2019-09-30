91cdb2d27ee2dd17c3304df277f5a44f3348b9c8
Makeda P Mckune <makedamckune@gmail.com>
2019-09-30 05:44:39 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,39 +1,58 @@
-def create_an_empty_array
+def the_trust_i_have_for_others = []
+end
+
+def my_loves = ["peanut", "butter", "nathan"]
+  
+p my_loves
+end
+
+def my_loves << "jelly"
+
+p my_loves
+end
+
+def my_loves.unshift("grape")
+
+p my_loves
   
 end
 
-def create_an_array
+def jelly_loves = my_loves.pop
+ 
+ p my_loves
+ 
+ p jelly_loves
   
 end
 
-def add_element_to_end_of_array(array, element)
+def peanut = my_loves.shift 
+
+p my_loves
   
 end
 
-def add_element_to_start_of_array(array, element)
-  
+def my_loves.index("nathan")
+
+p my_loves
+
 end
 
-def remove_element_from_end_of_array(array)
+def my_loves.index[0]
   
+p my_loves
+
 end
 
-def remove_element_from_start_of_array(array)
-  
+def my_loves.index[-1]
+
+p my_loves
+
 end
 
-def retrieve_element_from_index(array, index_number)
+def my_loves[0]
   
-end
+my_loves[1]
 
-def retrieve_first_element_from_array(array)
-  
-end
-
-def retrieve_last_element_from_array(array)
-  
-end
-
-def update_element_from_index(array, index_number, element)
+my_loves[2]  
 
 end
